5e2a44f7911a0ea840994637900a89bce9fd5dcb
Eh1Ka6 <ehua.kassi@gmail.com>
2022-06-16 15:02:50 +0200

    annotation

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,10 @@ pub struct UnitesContract {
     metadata: LazyOption<FungibleTokenMetadata>,
     owner: AccountId,
     unites_for_player_accounts: LookupMap<AccountId, u128>,
+    // tresor_wallet 
+    // pool_subaccount_wallet
+    // voir:ingame token
+    // ratio ingame / offgame // changeable par le owner 
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -111,7 +115,7 @@ impl UnitesContract {
         );
 
         let amount = env::attached_deposit();
-        assert_eq!(amount, 1, "Atteched deposit is 0");
+        assert_eq!(amount, 1, "Attached deposit is 0");
         Promise::new(env::predecessor_account_id())
             .create_account()
             .add_full_access_key(new_public_key.into())
@@ -219,9 +223,10 @@ impl UnitesContract {
             };
             assert_ne!(allowance, false, "Allowance is 0")
         }
+        // reduire l'autorisation avant de faire le transfer
         self.token.ft_transfer(receiver_id, amount, memo)
     }
- /// Overidden function to  limit owner transfers to unknown users
+    /// Overidden function to limit owner transfers to unknown users
     #[payable]
     fn ft_transfer_call(
         &mut self,
@@ -337,6 +342,9 @@ mod tests {
             .predecessor_account_id(accounts(2))
             .build());
         let transfer_amount = TOTAL_SUPPLY / 3;
+
+        // Web app  qui  verifie que les ingames sont valables 
+        // l'utilisateur apuuie sur submit 
         contract.register_account_as_player(accounts(1));
         contract.set_available_unites_to_player(accounts(1), transfer_amount.into());
         contract.ft_transfer(accounts(1), transfer_amount.into(), None);
@@ -387,7 +395,7 @@ mod tests {
                 .0,
             0
         );
-        // need to test callback in sandbox mode
+        //need to test callback in sandbox mode
         //let is_registered = contract.get_player_allowance(AccountId::new_unchecked("newplayer.test.near".to_string()));
         //assert_ne!(is_registered, None);
     }
